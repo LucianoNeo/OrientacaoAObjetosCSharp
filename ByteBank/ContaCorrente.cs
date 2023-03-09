e8a1b8c9677c62a5e7c515616efd253fb3d78501
Luciano Bueno <luciano.bueno@ingacode.com.br>
2023-03-09 10:34:01 -0300

    Add project files.

---
+++ ByteBank/ContaCorrente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            this.saldo -= valor;
+        }
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using bytebank;
+// tipo do objeto | nomeDaVariavel | new Classe
+ContaCorrente contaDoAndre = new ContaCorrente();
+contaDoAndre.titular = "André Silva";
+contaDoAndre.numero_agencia = 3051;
+contaDoAndre.conta = "1010-X";
+contaDoAndre.saldo = 500;
+
+
+
+Console.WriteLine("Saldo da conta: R$" + contaDoAndre.saldo);
+
+contaDoAndre.Depositar(100);
+
+Console.WriteLine("Saldo atualizado após depósito: R$" + contaDoAndre.saldo);
+
+contaDoAndre.Sacar(50);
+
+Console.WriteLine("Saldo atualizado após saque: R$" + contaDoAndre.saldo);
